3bf415a64f1a44cb94c288f58b2e9cd8eea45dad
canhuiHuang <75922065+canhuiHuang@users.noreply.github.com>
2021-03-12 01:24:03 -0800

    2

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import React, {Fragment} from 'react';
+import ContactState from './context/contact/ContactState';
+
+const App = () => {
+  return (
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="contaienr">
+            <Switch>
+              <Route exact path= '/' component = {Home} />
+              <Route exact path= '/about' component = {About} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
+  );
+}
+
+export default App;
